96ee0f969b20a8b493b052b7752edbe417d67101
Dan Spector <danno2423@gmail.com>
2020-12-17 18:16:03 -0500

    finished header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,29 +3,53 @@
 
 <style>
     .header {
+        align-items: center;
+        color: var(--light-gray);
         background-color: var(--dark-gray);
-        width: 100%;
+        display: flex;
+        flex-direction: column;
+        padding-top: var(--padding-desktop);
+        padding-bottom: var(--padding-desktop);
     }
 
     .header-content {
-        color: var(--light-gray);
         display: flex;
         justify-content: space-between;
-        width: 100%;
+        margin-bottom: calc(var(--base-spacing) * 2);
+        width: 80%;
+    }
+
+    .penny-image {
+        width: 450px;
+        height: 450px;
+    }
+
+    .title-container {
+        width: 635px;
     }
 
     .title {
+        font-family: "Libre Franklin", "Franklin Gothic", sans-serif;
+        font-weight: 700;
         font-size: 128px;
-        margin-bottom: 24px;
+        line-height: 128px;
     }
 
     .subhead {
+        font-family: var(--serif);
         font-size: 64px;
-        margin-bottom: 24px;
+        margin-bottom: calc(var(--base-spacing));
     }
 
     .byline {
+        font-family: var(--sans-serif);
         font-size: 24px;
+    }
+
+    .hook {
+        font-family: var(--serif);
+        font-size: 32px;
+        line-height: 40px;
     }
 </style>
 
@@ -33,9 +57,14 @@
     <div class="header-content">
         <img class="penny-image" src="assets/penny.png" alt="penny" />
         <div class="title-container">
-            <div class="title">In Pennies We Trust.</div>
+            <div class="title">In Pennies We Trust</div>
             <div class="subhead">But Should We?</div>
             <div class="byline">by Dan Spector</div>
         </div>
+    </div>
+    <div class="hook">
+        Millions of Americans still love the penny.
+        <br />
+        But are they aware of its drawbacks?
     </div>
 </div>
